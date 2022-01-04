1fa5c61a6d16ecb531bfefb56e70bc0f9e2399d2
Ryad <ryad22@live.fr>
2022-01-04 11:38:43 +0100

    mm

---
+++ script.js
@@ -0,0 +1,30 @@
+firstName = prompt("Nom?");
+let age = prompt("age?");
+
+if(firstName=="sofiane" && age>=18){
+    alert("c'est bav");
+}
+else{
+    alert("sorry");
+}
+
+if(firstName.length==age){
+    alert("yesss");
+}
+else{
+    alert("no");
+}
+
+if(firstName=="Abby" || firstName=="Paul" && age<70){
+    alert("succes");
+}
+else{
+    alert("pas loin");
+}
+
+if(age<50 && age>30){
+    alert("carré")
+}
+else{
+    alert("nope")
+}
